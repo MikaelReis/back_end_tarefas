e7748ef2f6c269f976feb0346405699881f4f9de
MikaelReis <mikaelreis08@gmail.com>
2025-06-02 14:33:34 -0300

    configurando serializers e views

--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+
+class TaskListCreateView(APIView):
+    def get(self, request):
+        tarefas = Task.objects.all()
+        serializer = TaskSerializer(tarefas, many= True)
+        return Response(serializer.data)
+    
+    def post (self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
