27843090d758bf0b079a279cb829ec58bb6c8bc6
MikaelReis <mikaelreis08@gmail.com>
2025-06-04 15:51:26 -0300

    realizando atualizações para o deploy

--- backend/tarefas/models.py
+++ backend/tarefas/models.py
@@ -7,6 +7,9 @@ class Task(models.Model):
     criada_em = models.DateTimeField(auto_now_add=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    criado_em = models.DateTimeField(auto_now_add=True)
+    data_limite = models.DateField(null=True, blank=True)
+
     def __str__(self):
         return self.titulo
 
